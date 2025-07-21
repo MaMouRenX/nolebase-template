af4177d4812d0826b1f2bfe01a0d8fc810074e7d
MaMouRenX <40289422@qq.com>
2025-07-21 17:49:04 +0800

    修改配置信息

--- metadata/index.ts
+++ metadata/index.ts
@@ -1,6 +1,6 @@
 /** 文本 */
-export const siteName = 'Nólëbase'
-export const siteShortName = 'Nólëbase'
+export const siteName = 'Mark'
+export const siteShortName = 'Mark'
 export const siteDescription = '记录回忆，知识和畅想的地方'
 
 /** 文档所在目录 */
